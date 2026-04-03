f77a0f047b1b20e8ec41a88de8e91fb329a3ad22
Yash Jangir <jangiryash04@gmail.com>
2026-04-03 19:06:58 +0530

    Initiating app.js

---
+++ backend/app.js
@@ -0,0 +1,19 @@
+const express = require("express") ;
+const app = express() ;
+const dotenv = require("dotenv") ;
+
+dotenv.config() ;
+
+const PORT = process.env.PORT ;
+
+app.get("/" , (req , res) => {
+    res.send("Hello World") ;
+})
+
+app.get("/health" , (req , res) => {
+    res.send("Server health : 100% ✅") ;
+})
+
+app.listen(PORT , () => {
+    console.log(`Server Running on PORT:${PORT} ✅`)
+})
