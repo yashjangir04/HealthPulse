cc34d8a4a258728700eb19d1dad0c314e1abce5a
Astha-codes16 <rekharanidk2017@gmail.com>
2026-04-03 22:11:42 +0530

    Added navlinks to menu items

---
+++ backend/models/appointment-model.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+
+const appointmentSchema = new mongoose.Schema({
+    roomID: {
+        type: String,
+        required: true
+    },
+    startTime: {
+        type: Date,
+        required: true
+    },
+    endTime: {
+        type: Date,
+        default: null
+    },
+    status: {
+        type: String,
+        enum: ["upcoming", "ongoing", "completed", "cancelled"],
+        default: "ongoing"
+    },
+    doctorID: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Doctor"
+    },
+    patientID: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Patient"
+    },
+    notes: {
+        type: String
+    },
+    prescribedMedicines: [
+        {
+            name: {
+                type: String,
+                required: true
+            },
+            dose: {
+                type: String,
+                default: ""
+            },
+            notes: {
+                type: String,
+                default: ""
+            }
+        }
+    ],
+    isActive: {
+        type: Boolean,
+        default: true
+    },
+    type: {
+        type: String,
+        enum: ["general", "scheduled"],
+        default: "general"
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model("Appointment", appointmentSchema);
